b01e9fc0b89201b4470a0aed8293802fbcbdcd5f
Carmine Massei <carminemassei@macbook-pro-di-carmine.local>
2017-01-27 15:43:25 +0100

    Initial Commit

---
+++ ProvaGit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ProvaGit
+//
+//  Created by Carmine Massei on 27/01/17.
+//  Copyright © 2017 iOSFoundation. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
